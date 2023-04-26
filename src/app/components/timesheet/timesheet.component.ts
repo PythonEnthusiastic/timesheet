62542c7b272c70a863c12b5150730be6c2d5dca3
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2023-04-26 09:01:34 -0600

    routing with parameters

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
   selector: 'app-timesheet',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./timesheet.component.scss']
 })
 export class TimesheetComponent {
+  departments!: Department[];
+  department!: Department;
 
+  constructor(
+    private route: ActivatedRoute,
+    private departmentsService: DepartmentsService
+  ) { }
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+  }
 }
