b2b5866f4f90368e5eda4f736707b8b638370433
Terry PythonEnthusiastic <terry.zambrano2508@stu.mtec.edu>
2023-05-01 10:16:45 -0600

    querying firestore collection

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -5,7 +5,8 @@ import { DepartmentsService } from 'src/app/services/departments.service';
 import { AbstractControl, ValidatorFn, FormControl } from '@angular/forms';
 import { Employee } from 'src/app/interfaces/employee';
 import { EmployeeService } from 'src/app/services/employee.service';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 
 @Component({
@@ -31,9 +32,15 @@ export class TimesheetComponent {
   ngOnInit(): void {
     this.$departments = this.departmentsService.getDepartments();
 
-    this.$departments.subscribe(x => {
-      this.department = x.find(department => department.id === this.route.snapshot.params['id']);
-    });;
+    this.$departments.pipe(
+      switchMap(departments => {
+        this.department = departments.find(dept => dept.id === this.route.snapshot.params['id'])
+        return this.employeeService.getEmployeeHoursByDepartmnet(this.department.id);
+      }),
+      tap(employees => {
+        this.employees = employees;
+      })
+    ).subscribe();
   }
 
   addEmployee(): void {
